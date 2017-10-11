603fc52c84ee496fc5a61e5ff4438fa56b0ac85a
USER <USER@INET-00>
2017-10-11 19:00:10 +0900

    Commit

---
+++ Chapter06/src/Ex06_08.java
@@ -0,0 +1,17 @@
+
+public class Ex06_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1~10까지의 합
+
+		int hap = 0;
+		int i;
+
+		for (i = 1; i <= 10; i = i++) {
+			hap += i;
+		}
+		System.out.printf("1~100까지 홀수 합 : %d", hap);
+	}
+
+}
---
+++ Chapter06/src/Ex06_09.java
@@ -0,0 +1,19 @@
+
+public class Ex06_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 500에서 1000까지의 홀수의 합
+
+		int hap = 0;
+		int i;
+
+		for (i = 501; i <= 1000; i = +2) {
+			hap += i;
+		}
+
+		System.out.printf("500~1000까지 홀수 합 %d", hap);
+
+	}
+
+}
