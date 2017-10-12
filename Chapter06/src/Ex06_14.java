94bc01bc02c84c3415ed4fa9fbc36e6a98b169cf
USER <USER@INET-00>
2017-10-12 18:21:11 +0900

    Commit

--- Chapter06/src/Ex06_14.java
+++ Chapter06/src/Ex06_14.java
@@ -4,8 +4,13 @@ public class Ex06_14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 2단~9단까지 출력하는 프로그램 충첩 for
-		
+		int i, k;
 
+		for (i = 2; i <= 9; i++) {
+			for (k = 1; k <= 9; k++) {
+				System.out.printf("%d X %d = %d\n", i, k, i * k);
+			}
+			System.out.printf("\n");
+		}
 	}
-
 }
