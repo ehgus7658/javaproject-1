b2f4180be9b5d6c8f4b8e751b7782444831f63fd
USER <USER@INET-00>
2017-10-12 17:59:01 +0900

    Commit

---
+++ Chapter06/src/Ex06_12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex06_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 구구단 for
+		Scanner s = new Scanner(System.in);
+		int i;
+		int dan;
+		System.out.printf("몇단? : ");
+		dan = s.nextInt();
+
+		for (i = 1; i <= 9; i++)
+			System.out.printf("%d X %d = %d\n", dan, i, dan * i);
+
+	}
+
+}
