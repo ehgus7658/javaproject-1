0b0e443d4627f11b3605b44759c33261e50b8732
USER <USER@INET-00>
2017-10-17 19:03:44 +0900

    Commit

--- Chapter07/src/Ex07_04.java
+++ Chapter07/src/Ex07_04.java
@@ -1,9 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Ex07_04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 
+		int a, b;
+		char ch;
+		while (true) {
+			System.out.print("더할 첫번째 수 입력 : ");
+			a = s.nextInt();
+			System.out.print("더할 두번째 수 입력 : ");
+			b = s.nextInt();
+			System.out.printf("계산할 연산자를 입력하세요 : ");
+			ch = (char) System.in.read();
+
+			switch (ch) {
+			case '+':
+				System.out.printf("%d + %d = %d", a, b, a + b);
+				break;
+			case '-':
+				System.out.printf("%d - %d = %d", a, b, a - b);
+				break;
+			case '*':
+				System.out.printf("%d * %d = %d", a, b, a * b);
+				break;
+			case '/':
+				System.out.printf("%d / %d = %d", a, b, a / (float) b);
+				break;
+			case '%':
+				System.out.printf("%d %% %d = %d", a, b, a % b);
+				break;
+			default:
+				System.out.printf("연산자를 잘못 입력했습니다.\n");
+			}
+		}
 	}
-
 }
