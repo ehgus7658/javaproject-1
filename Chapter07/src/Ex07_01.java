fc0a9323d3fee6a4f743c6909aa3eac6db036d4a
USER <USER@INET-00>
2017-10-17 17:38:08 +0900

    Commit

---
+++ Chapter07/src/Ex07_01.java
@@ -0,0 +1,18 @@
+
+public class Ex07_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int i=0;
+		while(i<5)
+		{
+			System.out.printf("While loop\n");
+			i++;
+		}*/
+		for(int i=0;i<5;i++)
+		{
+			System.out.printf("While loop\n");
+		}
+	}
+
+}
